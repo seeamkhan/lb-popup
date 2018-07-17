15a12a9f3fd2b6620d011d3b4bd7d8ac0ebcc15f
Abdul Quayum (Seeam) <seeamkhan@users.noreply.github.com>
2018-07-17 12:32:22 -0400

    This is enc-dec-pass script

---
+++ enc-dec-pass.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+import base64
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc))
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc)
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+secret = encode('pass', '@test123')
+back_to_original = decode('pass', secret)
+print secret
+print back_to_original
